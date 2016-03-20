7ad6a88d7b3edc4ce7e0fe01cb5746e87ba549de
Deepak Agrawal <dagrawal@yp.com>
2016-03-20 00:27:56 -0700

    Pascal Triangle and Bit Manipulation

---
+++ src/algo/PascalTriangle.java
@@ -0,0 +1,42 @@
+package algo;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+ 	Given numRows, generate the first numRows of Pascal's triangle.
+
+	For example, given numRows = 5,
+	Return
+
+	[
+	     [1],
+	    [1,1],
+	   [1,2,1],
+	  [1,3,3,1],
+	 [1,4,6,4,1]
+	]
+ */
+
+public class PascalTriangle {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (numRows == 0)
+			return result;
+		List<Integer> first = new ArrayList<Integer>();
+		first.add(1);
+		result.add(first);
+		for (int i = 1; i < numRows; i++) {
+			List<Integer> previous = result.get(i - 1);
+			List<Integer> curr = new ArrayList<Integer>();
+			curr.add(previous.get(0));
+			int previousEl = previous.get(0);
+			for (int j = 1; j < previous.size(); j++) {
+				curr.add(previousEl + previous.get(j));
+				previousEl = previous.get(j);
+			}
+			curr.add(previous.get(previous.size() - 1));
+			result.add(curr);
+		}
+		return result;
+	}
+}
---
+++ src/math/BitManipulation.java
@@ -0,0 +1,35 @@
+package math;
+
+public class BitManipulation {
+	public static int reverseBits(int n) {
+		int m = 0;
+		int tmp = n;
+		for (int i = 0; i < 32; i++) {
+			m = (m << 1) | (tmp & 1);
+			tmp >>>= 1;
+		}
+		return m;
+	}
+
+	
+	/*
+	 * Number of one's in an integer
+	 */
+	public static int hammingWeight(int n) {
+		int count = 0;
+		while (n > 0) {
+			int t = n & 1;
+			if (t == 1) {
+				count++;
+			}
+			n >>>= 1;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(reverseBits(4));
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(hammingWeight(Integer.MAX_VALUE));
+	}
+}
